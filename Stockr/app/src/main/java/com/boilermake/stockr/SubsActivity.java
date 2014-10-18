9246df404c2f89e4988cc192e2f478bc3a212c7b
Inhwan Eric Lee <lee23@purdue.edu>
2014-10-18 16:52:58 -0400

    subs activity functions are being added

--- Stockr/app/src/main/java/com/boilermake/stockr/SubsActivity.java
+++ Stockr/app/src/main/java/com/boilermake/stockr/SubsActivity.java
@@ -1,26 +1,116 @@
 package com.boilermake.stockr;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 
 public class SubsActivity extends Activity {
+
+    Button instant_button;
+    Button spatial_button;
+    Button time_button;
+    Button greater_button;
+    Button lesser_button;
+    Button company_button;
+    EditText input_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subs);
         Animation slideup = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slideup);
-        RelativeLayout layout = (RelativeLayout)findViewById(R.id.subs_layout);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.subs_layout);
 
         layout.startAnimation(slideup);
         layout.setVisibility(View.VISIBLE);
+
+        //Components initializing
+        instant_button = (Button) findViewById(R.id.instant_button);
+        spatial_button = (Button) findViewById(R.id.spatial_button);
+        time_button = (Button) findViewById(R.id.time_button);
+        greater_button = (Button) findViewById(R.id.greater_button);
+        lesser_button = (Button) findViewById(R.id.lesser_button);
+        company_button = (Button) findViewById(R.id.company_button);
+        input_value = (EditText) findViewById(R.id.input_value);
+
+        //Event Listener
+
+        //1. Type Buttons
+        instant_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                instant_button.setBackground(getResources().getDrawable(R.drawable.blue_shape));
+                instant_button.setTextColor(getResources().getColor(R.color.light_grey));
+
+                spatial_button.setBackgroundColor(Color.WHITE);
+                spatial_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+                time_button.setBackgroundColor(Color.WHITE);
+                time_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+            }
+        });
+        spatial_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spatial_button.setBackground(getResources().getDrawable(R.drawable.blue_shape));
+                spatial_button.setTextColor(getResources().getColor(R.color.light_grey));
+
+                instant_button.setBackgroundColor(Color.WHITE);
+                instant_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+                time_button.setBackgroundColor(Color.WHITE);
+                time_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+            }
+        });
+        time_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                time_button.setBackground(getResources().getDrawable(R.drawable.blue_shape));
+                time_button.setTextColor(getResources().getColor(R.color.light_grey));
+
+                spatial_button.setBackgroundColor(Color.WHITE);
+                spatial_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+
+                instant_button.setBackgroundColor(Color.WHITE);
+                instant_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+            }
+        });
+
+
+        //2. Association Buttons
+        greater_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                greater_button.setBackground(getResources().getDrawable(R.drawable.blue_shape));
+                greater_button.setTextColor(getResources().getColor(R.color.light_grey));
+
+                lesser_button.setBackgroundColor(Color.WHITE);
+                lesser_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+            }
+        });
+        lesser_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lesser_button.setBackground(getResources().getDrawable(R.drawable.blue_shape));
+                lesser_button.setTextColor(getResources().getColor(R.color.light_grey));
+
+                greater_button.setBackgroundColor(Color.WHITE);
+                greater_button.setTextColor(getResources().getColor(R.color.heavy_grey));
+
+            }
+        });
     }
 
 
