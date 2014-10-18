40ac8d4b6420af75f4294c9b8d710463e0172d5e
Inhwan Eric Lee <lee23@purdue.edu>
2014-10-18 02:44:35 -0400

    Activities are set up

--- Stockr/app/src/main/java/com/boilermake/stockr/IntroActivity.java
+++ Stockr/app/src/main/java/com/boilermake/stockr/IntroActivity.java
@@ -1,19 +1,40 @@
 package com.boilermake.stockr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class IntroActivity extends Activity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Full page logo screen - remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_intro);
+
+        // Delay for 1 sec
+        handler = new Handler();
+        handler.postDelayed(irun, 1200);
     }
 
+    Runnable irun = new Runnable() {
+        public void run() {
+            Intent i = new Intent(IntroActivity.this, BoardActivity.class);
+            startActivity(i);
+            finish();
+
+            // Screen fade in & out effect
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,6 +43,10 @@ public class IntroActivity extends Activity {
         return true;
     }
 
+    public void onBackPressed() {
+        super.onBackPressed();
+        handler.removeCallbacks(irun);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
